a039635ccd71f92acb62536145f0c3fd9629134f
zhaoyudragon <zhaoyudragon@gmail.com>
2020-03-20 13:13:20 +0800

    禁用了eslint

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/logo.png" />
+    <h1>哈哈哈</h1>
+    <h2>哈哈哈ffff</h2>
+    <h3>嘿嘿嘿嘿hei</h3>
+    <input value="请输入内容" />
+    <input value="请输入内容哦" />
+
     <div>
       <p>
         If Element is successfully added to this project, you'll see an
@@ -9,24 +15,24 @@
       </p>
       <el-button>el-button</el-button>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
