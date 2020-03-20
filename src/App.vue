6913786b4bd0682b90ed3dd44176e5ded57bd742
zhaoyudragon <zhaoyudragon@gmail.com>
2020-03-20 15:38:39 +0800

    不知道提交了啥

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,10 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" />
-    <h1>哈哈哈</h1>
-    <h2>哈哈哈ffff</h2>
-    <h3>嘿嘿嘿嘿hei</h3>
-    <input value="请输入内容" />
-    <input value="请输入内容哦" />
-
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <router-link to="/">Home</router-link>&nbsp;&nbsp;&nbsp;
+    <router-link to="/about">About</router-link>
+    <router-view />
   </div>
 </template>
-
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
-};
-</script>
 
 <style>
 #app {
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <img src="../../../hello_ui_elementui/src/assets/logo.png" alt="">
   </div>
 </template>
